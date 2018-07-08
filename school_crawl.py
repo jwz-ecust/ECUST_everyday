86e20f7b73b17f6eadcad2ca9425eca0601335ef
jwzhang <aaron.zhang@hujiang.com>
2018-07-08 10:40:27 +0800

    utf error

--- school_crawl.py
+++ school_crawl.py
@@ -61,7 +61,12 @@ def send_mail(receiver, content, name):
     sender_qq = parser.get('DEFAULT', 'send_id')
     sender_qq_mail = parser.get('DEFAULT', 'send_mail')
     pwd = os.environ['PASSWORD']
-    mail_title = parser.get('DEFAULT', 'title') + u'\t'  +  name.decode('utf-8')
+    try:
+        username = name.decode('utf-8')
+    except:
+        username = name
+
+    mail_title = parser.get('DEFAULT', 'title') + u'\t' + username
     smtp = SMTP_SSL(host_server)
     smtp.login(sender_qq, pwd)
     msg = MIMEText(content, "html", 'utf-8')
@@ -87,10 +92,9 @@ for node in soup_notifications.find_all(attrs={'class': re.compile('list_item i\
         line = u"{}\t[{}]({})\n".format(note_date, note_title, note_link)
         content += line
         # print line
+
 content += '\n\n\n\n---\n'
-#####################################################################
 # 校园要闻
-# print(u'校园要闻')
 content += u'## 校园新闻\n'
 news_url = 'https://news.ecust.edu.cn/news?important=0'
 soup_news = get_req(news_url)
@@ -106,8 +110,6 @@ for node in newscontent.find_all('a'):
 content += '\n\n\n\n---\n'
 
 # 校园快讯
-# print('##################################################################')
-# print(u'校园快讯')
 content += u'## 校园快讯\n'
 fast_url = 'https://news.ecust.edu.cn/news?important=1'
 soup_fast = get_req(fast_url)
@@ -121,12 +123,9 @@ for node in fastcontent.find_all('a'):
         content += line
         # print line
 
-##################################################################
 # 学术报告
 content += '\n\n\n\n---\n'
 content += u'## 学术报告\n'
-# print('##################################################################')
-# print(u'学术报告')
 reports_url = 'https://news.ecust.edu.cn/reports'
 soup_reports = get_req(reports_url)
 reportcontent = soup_reports.find(class_='content').ul
