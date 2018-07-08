247ad595e5009e7cd045337594e2b766beccc437
jwzhang <aaron.zhang@hujiang.com>
2018-07-08 10:16:09 +0800

    fix school crawl mistake

--- school_crawl.py
+++ school_crawl.py
@@ -1,16 +1,20 @@
-w# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import requests
 import time
-from lxml import etree
 import sys
 import re
+import os
 import mistune
+import ConfigParser
 import smtplib
 from smtplib import SMTP_SSL
 from email.header import Header
 from email.mime.text import MIMEText
+from bs4 import BeautifulSoup
 reload(sys)
 sys.setdefaultencoding('utf-8')
+parser = ConfigParser.ConfigParser()
+parser.read('./crawl.conf')
 
 
 def get_timestamp_from_string(time_string):
@@ -31,7 +35,8 @@ def is_new_for_today(timestr, delta_time=1.0):
 
 
 def enter2space(title):
-    result = re.sub('\n', '\s', title)
+    result = re.sub('[\n]', '', title)
+    result = re.sub('\s+', ' ', result)
     return result
 
 
@@ -41,15 +46,18 @@ def get_req(url):
     }
     req = requests.get(url, headers=headers)
     content = req.content
-    tree = etree.HTML(content)
-    return tree
+    #  tree = etree.HTML(content)
+    soup = BeautifulSoup(content, 'html.parser')
+    #  return tree
+    return soup
 
-def send_mail(receiver, content):
-    host_server = 'smtp.qq.com'
-    sender_qq = "897978644"
-    sender_qq_mail = "897978644@qq.com"
-    pwd = 'ppnovemkpapebecf'
-    mail_title = u'ECUST'
+
+def send_mail(receiver, content, name):
+    host_server = parser.get('DEFAULT', 'host_server')
+    sender_qq = parser.get('DEFAULT', 'send_id')
+    sender_qq_mail = parser.get('DEFAULT', 'send_mail')
+    pwd = os.environ['PASSWORD']
+    mail_title = parser.get('DEFAULT', 'title') + u'\t'  +  name.decode('utf-8')
     smtp = SMTP_SSL(host_server)
     smtp.login(sender_qq, pwd)
     msg = MIMEText(content, "html", 'utf-8')
@@ -61,69 +69,81 @@ def send_mail(receiver, content):
 
 
 content = u''
-
-# notifications
-# daily_md = open('./schools.md', 'w')
-
+# 研究生院通知
 content += u'## 研究生院通知\n'
-# daily_md.write(u'## 研究生院通知\n')
-
 url_notifications = 'http://gschool.ecust.edu.cn/tzgg1/list.htm'
-tree_notifications = get_req(url_notifications)
-for node in tree_notifications.xpath('//*[starts-with(@class, "list_item")]'):
-    # http://gschool.ecust.edu.cn/2018/0604/c8139a76807/page.htm
-    note_title = node.xpath('./div/span[2]/a/text()')[0].strip()
-    note_title = enter2space(note_title)
-    note_link = 'http://gschool.ecust.edu.cn' + node.xpath('./div[1]/span/a/@href')[0]
-    note_date = node.xpath('./div[2]/span/text()')[0]
+soup_notifications = get_req(url_notifications)
+for node in soup_notifications.find_all(attrs={'class': re.compile('list_item i\d+')}):
+    note_ = node.find(attrs={'class': 'Article_Title'})
+    note_title = note_.a.text
+    note_link = 'http://gschool.ecust.edu.cn' + note_.a['href']
+    note_date = node.find(attrs={'class': 'Article_PublishDate'}).text
+    # print note_title, note_link, note_date
     if is_new_for_today(note_date):
         line = u"{}\t[{}]({})\n".format(note_date, note_title, note_link)
-        daily_md.write(line)
         content += line
         # print line
-
-# daily_md.write('\n\n\n\n---\n')
 content += '\n\n\n\n---\n'
-
+#####################################################################
+# 校园要闻
+# print(u'校园要闻')
 content += u'## 校园新闻\n'
-# daily_md.write(u'## 校园新闻\n')
-# news
 news_url = 'https://news.ecust.edu.cn/news?important=0'
-tree_news = get_req(news_url)
-for node in tree_news.xpath('//div[@class="content"]/ul/li/a'):
-    news_time = node.xpath('./span[1]/text()')[0].strip()
-    news_title = node.xpath('./span[3]/text()')[0].strip()
-    news_title = enter2space(news_title)
-    news_link = "https://news.ecust.edu.cn" + node.xpath('./@href')[0]
+soup_news = get_req(news_url)
+newscontent = soup_news.find(class_='content').ul
+for node in newscontent.find_all('a'):
+    news_link = "https://news.ecust.edu.cn" + node['href']
+    news_time = node.find(class_='time').text.strip()
+    news_title = node.find_all('span')[-1].text.strip()
     if is_new_for_today(news_time):
         line = u"{}\t[{}]({})\n".format(news_time, news_title, news_link)
-        # daily_md.write(line)
+        content += line
+        # print line
+content += '\n\n\n\n---\n'
+
+# 校园快讯
+# print('##################################################################')
+# print(u'校园快讯')
+content += u'## 校园快讯\n'
+fast_url = 'https://news.ecust.edu.cn/news?important=1'
+soup_fast = get_req(fast_url)
+fastcontent = soup_fast.find(class_='content').ul
+for node in fastcontent.find_all('a'):
+    fast_link = "https://news.ecust.edu.cn" + node['href']
+    fast_time = node.find(class_='time').text.strip()
+    fast_title = node.find_all('span')[-1].text.strip()
+    if is_new_for_today(fast_time):
+        line = u"{}\t[{}]({})\n".format(fast_time, fast_title, fast_link)
         content += line
         # print line
 
-# daily_md.write('\n\n\n\n---\n')
-# daily_md.write(u'## 学术报告\n')
+##################################################################
+# 学术报告
 content += '\n\n\n\n---\n'
 content += u'## 学术报告\n'
-# reports
+# print('##################################################################')
+# print(u'学术报告')
 reports_url = 'https://news.ecust.edu.cn/reports'
-tree_reports = get_req(reports_url)
-for node in tree_reports.xpath('//div[@class="content"]/ul/li/a'):
-    report_time = node.xpath('./span[1]/text()')[0].strip().split(u'：')[-1].strip()
-    report_title = node.xpath('./span[3]/text()')[0].strip()
+soup_reports = get_req(reports_url)
+reportcontent = soup_reports.find(class_='content').ul
+for node in reportcontent.find_all('a'):
+    report_link = "https://news.ecust.edu.cn" + node['href']
+    report_time = node.find(class_='time').text.strip().split(u'：')[-1].strip()
+    report_title = node.find_all('span')[-1].text.strip()
     report_title = enter2space(report_title)
-    report_link = "https://news.ecust.edu.cn" + node.xpath('./@href')[0]
+
     if is_new_for_today(report_time):
         line = u"{}\t[{}]({})\n".format(report_time, report_title, report_link)
-        # daily_md.write(line)
         content += line
         # print line
-
-# daily_md.close()
 
 # content convert to html format
 html = mistune.markdown(content, escape=True, hard_wrap=True)
-# html = open('zjw.html', 'w').write(html)
 
 # send mail
-send_mail('aaronzjw6@gmail.com', html)
+for user in parser.sections():
+    name = parser.get(user, 'name')
+    receiver = parser.get(user, 'receiver')
+    send_mail(receiver, html, name)
+
+print('Sending Successfully')
